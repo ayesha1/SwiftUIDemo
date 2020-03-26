6ee88b6dc286162a56c08e81eaa1171920ef75f7
Ayesha Khan <akhan1231@hotmail.com>
2020-03-25 19:06:33 -0700

    Creating and Combining Views finished

--- AppleTutorial/ContentView.swift
+++ AppleTutorial/ContentView.swift
@@ -12,10 +12,13 @@ struct ContentView: View {
     var body: some View {
         VStack {
             MapView()
+                .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
+            
             CircleImage()
                 .offset(y:-130)
                 .padding(.bottom, -130)
+            
             VStack(alignment: .leading) {
                 Text("Tutle Rock")
                     .font(.title)
@@ -28,6 +31,8 @@ struct ContentView: View {
                 }
             }
             .padding()
+            
+            Spacer()
         }
     }
 }
