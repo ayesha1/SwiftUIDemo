f6cd64fe8c45d8bdb302e60035b03bb4eb9bb559
Ayesha Khan <akhan1231@hotmail.com>
2020-03-25 20:03:55 -0700

    Completed making a list of landmarks

---
+++ AppleTutorial/LandmarkList.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkList.swift
+//  AppleTutorial
+//
+//  Created by Ayesha Khan on 3/25/20.
+//  Copyright © 2020 Ayesha Khan. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
---
+++ AppleTutorial/LandmarkRow.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkRow.swift
+//  AppleTutorial
+//
+//  Created by Ayesha Khan on 3/25/20.
+//  Copyright © 2020 Ayesha Khan. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+        
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
